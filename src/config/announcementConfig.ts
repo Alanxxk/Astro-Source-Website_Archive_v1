5c1cb037bd1821340fce06d17fd715289aa73f24
Alan_xxk <2380776513@qq.com>
2026-07-10 23:50:46 +0800

    archive

--- src/config/announcementConfig.ts
+++ src/config/announcementConfig.ts
@@ -2,7 +2,7 @@ import type { AnnouncementConfig } from "../types/config";
 
 export const announcementConfig: AnnouncementConfig = {
   title: "通知公告", // 公告标题
-  content: "欢迎来到Alanxxk的博客！觉得本站内容不错的话，请给我多多反馈哟~", // 公告内容
+  content: "欢迎来到Alanxxk的博客！觉得本站内容不错的话，请给我多多反馈哟~（已停止维护该纪念网站）", // 公告内容
   closable: false, // 允许用户关闭公告
   link: {
     enable: true, // 启用链接
